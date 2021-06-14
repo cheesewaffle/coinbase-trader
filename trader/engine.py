dbc6e55a3e86ba81342d02fbab56b62de031e95b
cheesewaffle <huntermyles00@gmail.com>
2021-06-13 21:10:59 -0500

    engine update, fixed timezone and some small bugs

--- trader/engine.py
+++ trader/engine.py
@@ -1,87 +1,81 @@
 from datetime import datetime, timedelta
-import time, json, calendar
+import time
+import json
+import calendar
+import config
 import cbpro
-#Credentials
-key = '8da2e3ada96a6fee893ad8750e0f341d'
-secret = 'JS/IioxL15Lg3liGWYMiHAOF8ZFXq1ie6met1ed1++T61ImuJeo93Sjk7S2xkQ8LCmDDq36hRYz2FcuChp/HSw=='
-passphrase = 'k8f4r1czu8s'
-sandbox = 'https://api-public.sandbox.pro.coinbase.com'
-#Clients
+
+# Credentials
+key = config.key
+secret = config.secret
+passphrase = config.passphrase
+sandbox = config.sandbox
+
+# Clients
 messenger = cbpro.Messenger()
 public = cbpro.PublicClient(messenger)
-
 private = cbpro.private_client(key, secret, passphrase, sandbox)
 
-activeSymbols = [] 
+activeSymbols = []
 
-def get_all_symbols():
+def get_all_symbols():  # Grabs all symbols from coinbase
     time.sleep(0.26)
-    symbols = [] 
+    symbols = []
     products = public.products.list()
     for product in products:
         symbol = (product['id'])
-        if 'USD' in symbol and 'USDC' not in symbol and 'USDT' not in symbol:
+        if 'USD' in symbol and 'USDC' not in symbol and 'USDT' not in symbol and 'DAI' not in symbol:
             symbols.append(symbol)
-        else:
-            pass
 
     return symbols
 
-def getCurrentPrice(symbol):
+def getCurrentPrice(symbol):  # Grabs current price for symbol
     time.sleep(0.26)
     priceDict = public.products.stats(symbol)
     price = priceDict['last']
-    return price 
+    return price
 
-def check_watch_list(symbol, currentPrice):
+def check_watch_list(symbol, currentPrice):  # 60 Day - check if 50% decrease
     try:
         currentTime = datetime.now()
-        timeData = currentTime + timedelta(days = -60)
-        start = timeData.strftime("%Y-%m-%dT") + '00:00:00'
-        granularity = 60
+        timeData = currentTime + timedelta(hours=-1435) # Timezone offset factored in -5 hours
+        requestedTime = timeData.strftime("%Y-%m-%dT%H:%M:00")
 
-        params = {'start': start, 'end': start, 'granularity': granularity} 
+        params = {'start': requestedTime,
+                  'end': requestedTime, 'granularity': 60}
         history = public.products.history(symbol, params)
         time.sleep(0.26)
-
-        product_id = symbol # 'BTC-USD'
 
         priceCheck = history[0][3]
 
         difference = abs(1 - (float(currentPrice) / float(priceCheck)))
-        
 
         if difference > 0.5:
             status = False
+            print(f'{symbol} not watch...')
         elif difference < 0.5:
             status = True
-
-        print('-------------')
-        print(symbol)
-        print(difference)
-        print(currentPrice)
-        print(priceCheck)
-        print(status)
-        print('-------------')
+            print('-------------')
+            print(
+                f'CheckWatch\nSymbol: {symbol}\nDifference: {difference}\nCurrent Price: {currentPrice}\nPrice Check: {priceCheck}\nStatus: {status}\nHistory: {history}')
+            print('-------------')
 
         return status
-    
+
     except:
-        print(symbol)
-        print("isWatch Check Failed -  - Historical Data not found from Coinbase")
+        print(
+            f"{symbol}\nisWatch Check Failed -  - Historical Data not found from Coinbase")
         return False
 
-def check_flat(symbol, currentPrice):
+def check_flat(symbol, currentPrice):  # 48 Hour - check if withing 1%
     try:
         status = False
-
         currentTime = datetime.now()
-        timeData = currentTime + timedelta(days = -2)
-        start = timeData.strftime("%Y-%m-%dT") + '00:00:00'
+        timeData = currentTime + timedelta(hours=-43) # Timezone offset factored in -5 hours 
+        requestedTime = timeData.strftime("%Y-%m-%dT%H:%M:00")
 
-        granularity = 60
-
-        params = {'start': start, 'end': start, 'granularity': granularity} 
+        params = {'start': requestedTime,
+                  'end': requestedTime, 'granularity': 60}
         history = public.products.history(symbol, params)
         time.sleep(0.26)
 
@@ -91,28 +85,24 @@ def check_flat(symbol, currentPrice):
 
         if difference > 0.01:
             status = False
+            print(f'{symbol} not flat...')
         elif difference < 0.01:
             status = True
-
-        print('-------------')
-        print(symbol)
-        print(difference)
-        print(currentPrice)
-        print(priceCheck)
-        print(status)
-        print('-------------')
+            print('-------------')
+            print(
+                f'CheckFlat\nSymbol: {symbol}\nDifference: {difference}\nCurrent Price: {currentPrice}\nPrice Check: {priceCheck}\nStatus: {status}')
+            print('-------------')
 
         return status
-        
 
     except:
-        print(symbol)
-        print("isFlat Check Failed - Historical Data not found from Coinbase")
+        print(
+            f"{symbol}\nisFlat Check Failed - Historical Data not found from Coinbase")
         return False
 
-def buy(symbol, amount):
+def buy(symbol, amount):  # Buy at market price
     market = {
-        'funds': amount, # may need to have decimal
+        'funds': amount,  # may need to have decimal
         'side': 'buy',
         'product_id': symbol,
         'type': 'market',
@@ -120,9 +110,9 @@ def buy(symbol, amount):
     response = private.orders.post(market)
     print(response)
 
-def sell(symbol, size):
+def sell(symbol, size):  # Sell at market price
     market = {
-        'size': size, # may need to have decimal
+        'size': size,  # may need to have decimal
         'side': 'sell',
         'product_id': symbol,
         'type': 'market',
@@ -130,12 +120,11 @@ def sell(symbol, size):
     response = private.orders.post(market)
     print(response)
 
-def get_active_wallets():
-    activeWallets = []
-    activeSymbols = []
-    wallets = private.accounts.list() 
-    time.sleep(0.25)
-    
+def get_active_wallets():  # Grabs all wallets from account that are a USD pair
+    activeWallets, activeSymbols = [], []
+    wallets = private.accounts.list()
+    time.sleep(0.26)
+
     for wallet in wallets:
         balance = float(wallet.get('balance'))
         if balance > 0:
@@ -146,14 +135,15 @@ def get_active_wallets():
             activeWallets.append(activeWallet)
 
     return activeWallets, activeSymbols
-            
+
 def filter_fill_data(symbol):
-    filteredFills = [] 
+    filteredFills = []
     product_id = {'product_id': symbol}
     fills = private.fills.list(product_id)
-    for fill in fills:# {'created_at': '2021-05-22T19:45:15.07Z', 'trade_id': 29912427, 'product_id': 'BTC-USD', 'order_id': 'fc68d9c7-662c-435a-8e07-152fb9c3927e', 'user_id': '5d0c40c934ae30321551b72f', 'profile_id': '867ccf0a-6475-43ae-95ce-fe0bcf2316ea', 'liquidity': 'T', 'price': '37915.85000000', 'size': '0.00210487', 'fee': '0.3990396759475000', 'side': 'buy', 'settled': True, 'usd_volume': '79.8079351895000000'}
+    for fill in fills:  # {'created_at': '2021-05-22T19:45:15.07Z', 'trade_id': 29912427, 'product_id': 'BTC-USD', 'order_id': 'fc68d9c7-662c-435a-8e07-152fb9c3927e', 'user_id': '5d0c40c934ae30321551b72f', 'profile_id': '867ccf0a-6475-43ae-95ce-fe0bcf2316ea', 'liquidity': 'T', 'price': '37915.85000000', 'size': '0.00210487', 'fee': '0.3990396759475000', 'side': 'buy', 'settled': True, 'usd_volume': '79.8079351895000000'}
         timedata = fill.get('created_at')
-        time = calendar.timegm(datetime.strptime(timedata, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple())
+        time = calendar.timegm(datetime.strptime(
+            timedata, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple())
         symbol = fill.get('product_id')
         price = fill.get('price')
         size = fill.get('size')
@@ -161,10 +151,10 @@ def filter_fill_data(symbol):
 
         if side == 'buy':
             filteredFills.append([time, symbol, price, size])
-            
-    recentFills = []
-    usedSymbols = []
-    for item in filteredFills: 
+
+    recentFills, usedSymbols = [], []
+
+    for item in filteredFills:
         if item[1] not in usedSymbols:
             recentFills.append(item)
             usedSymbols.append(item[1])
@@ -174,9 +164,9 @@ def filter_fill_data(symbol):
 def check_sell_condition(symbols: list):
     currentSymbols = []
     message = cbpro.get_message({
-    'type': 'subscribe',
-    'product_ids': symbols, 
-    'channels': ['ticker']
+        'type': 'subscribe',
+        'product_ids': symbols,
+        'channels': ['ticker']
     })
 
     header = cbpro.WebsocketHeader(key, secret, passphrase)
@@ -203,12 +193,19 @@ def check_sell_condition(symbols: list):
     stream.disconnect()
 
     for j in currentSymbols:
+        print(j)
         marketSymbol = j.get('symbol')
         price = j.get("price")
-        fillData = filter_fill_data(marketSymbol) # [1621869301, 'BTC-USD', '37672.65000000', '0.00264123']
+        # [1621869301, 'BTC-USD', '37672.65000000', '0.00264123']
+        fillData = filter_fill_data(marketSymbol)
         priceDifference = float(fillData[2]) - float(price)
 
-        valueChange = priceDifference - float(fillData[2])
+        valueChange = abs(float(fillData[2])) - abs(priceDifference)
+
+        print(f"fillData: {float(fillData[2])}")
+        print(f"price: {price}")
+        print(f"priceDifference: {priceDifference}")
+        print(f"valuechange: {valueChange}")
 
         if valueChange > 0.05:
             print(f'SELL {marketSymbol}')
@@ -218,27 +215,28 @@ def check_sell_condition(symbols: list):
             continue
 
 def main():
+    archive = []
     start = time.time()
     print('starting...')
     optimalTrades = []
     symbols = get_all_symbols()
     activeWallets, activeSymbols = get_active_wallets()
-    check_sell_condition(activeSymbols)
+    check_sell_condition(activeSymbols) # check this 
     for symbol in symbols:
+        isWatch, isFlat = False, False
         currentPrice = getCurrentPrice(symbol)
         isFlat = check_flat(symbol, currentPrice)
 
         if isFlat == True:
             isWatch = check_watch_list(symbol, currentPrice)
-        else:
-            continue
 
         if isFlat == True and isWatch == True:
             if symbol not in activeSymbols:
                 print(f"BUY {symbol}")
                 buy(symbol, '200')
-
+                archive.append([symbol, currentPrice, isFlat, isWatch])
+        
+    print(archive)
     print(f'Time to complete: {time.time() - start}')
 
-
-# main()
+main()
